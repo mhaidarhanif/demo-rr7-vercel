00b1be205d79b043d2a9ed543d401831428ae01a
M Haidar Hanif <git@mhaidarhanif.com>
2025-01-14 23:45:09 +0700

    fix: esm

--- app/lib/database.server.ts
+++ app/lib/database.server.ts
@@ -1,3 +1,10 @@
-import { PrismaClient } from "@prisma/client-generated";
+import * as Prisma from "@prisma/client";
+
+import { default as ProdPrisma } from "@prisma/client";
+
+let { PrismaClient } = Prisma;
+
+if (process.env.NODE_ENV === "production")
+  PrismaClient = ProdPrisma.PrismaClient;
 
 export const prisma = new PrismaClient();
