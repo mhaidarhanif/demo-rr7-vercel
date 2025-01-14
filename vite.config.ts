8e67bd9ac6b2d8c2bbeed7aee2019c3280d9327a
M Haidar Hanif <git@mhaidarhanif.com>
2025-01-14 23:23:13 +0700

    fix: vite

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,6 @@ export default defineConfig(({ isSsrBuild, command }) => ({
     rollupOptions: isSsrBuild
       ? {
           input: "./server/app.ts",
-          external: ["@prisma/client"],
         }
       : undefined,
   },
