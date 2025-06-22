3a3d656d9550a75c8fd3a396db99a3e204e90723
M Haidar Hanif <git@mhaidarhanif.com>
2025-06-23 01:24:11 +0700

    feat: loaders

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,10 +2,11 @@ import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+  return [{ title: "New React Router App" }, { name: "description", content: "Welcome to React Router!" }];
+}
+
+export function loader() {
+  return "home";
 }
 
 export default function Home() {
